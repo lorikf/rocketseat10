04fe1dece5f475aec7237f6feb3719efc0beb2e1
Lorenzo Fiore <lorifiore97@gmail.com>
2020-01-19 19:15:40 -0300

    added cors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose')
 const routes = require('./routes')
+const cors = require('cors')
 
 const app = express();
 
@@ -8,7 +9,7 @@ mongoose.connect('mongodb+srv://lorenzo:lkf@cluster0-apmg6.mongodb.net/week10?re
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
-
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 
